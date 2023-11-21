3ac33ce7459b0f7022a6cf1fddcb24f754c0dc19
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-21 10:58:07 +0300

    Add variables

--- main.tf
+++ main.tf
@@ -1,9 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-key20231117084134961500000001"
+    key = "Terraform-loop/users/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-2"
 }
 
+variable "iam_users" {
+    description = "List of iam users"
+    type = list(string)
+    default = [ "Abebe","Beqele","Kebed" ]
+  
+}
+
 resource "aws_iam_user" "target_group_arns" {
     count = 3
-    name = "test-user${count.index}"
-  
+    name = var.iam_users[count.index]
+      
 }
